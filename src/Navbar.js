aeb09efff85d232a128df2206eeb5e97edb93b8a
AOS2019 <samuel90ezekiel@gmail.com>
2022-09-16 13:11:05 +0100

    Navbar boilerplate

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+   return ( 
+      <div className="navbar">
+         <h1> logo image </h1>
+         <div className="links">
+            <a href='/'> Home </a>
+            <a href='/create'> New Blog </a>
+         </div>
+      </div>
+    );
+}
+ 
+export default Navbar;
