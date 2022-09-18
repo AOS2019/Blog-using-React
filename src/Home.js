495767ac453ff5bf2c34001cfd8b63b96aa103d2
AOS2019 <samuel90ezekiel@gmail.com>
2022-09-18 16:28:56 +0100

    custom hook: useFetech

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,18 @@
-import { useState } from 'react';
+// import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
-import BlogListTemp from './BlogListTemp';
+import useFetch from './useFetch';
+// import BlogListTemp from './BlogListTemp';
 
 const Home = () => {
-   const [blogs, setBlogs] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ])
-
-   const handleDelete = (id) => {
-      const newBlogs = blogs.filter(blogs => blogs.id !== id);
-      setBlogs(newBlogs);
-
-   }
+   const {data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
 
    return ( 
       <div className="home">
-         <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
-         <BlogListTemp blogs={blogs.filter((blogs) => blogs.author === "mario")} title="Mario's Blogs"/>
+         {/* {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>} */}
+         {error && <div className='error'> <h3> {error} </h3> </div>}
+         {isPending && <div className='loading'> <h2> Loading... </h2> </div>}
+         {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
+         {/* <BlogListTemp blogs={blogs.filter((blogs) => blogs.author === "mario")} title="Mario's Blogs"/> */}
       </div>
     );
 }
